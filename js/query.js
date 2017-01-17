ac6e7189b2525c70092b9467fc34485d015ab733
tsmanolov <ts.manolov@gmail.com>
2017-01-17 09:55:56 +0200

    export functionality added

--- js/query.js
+++ js/query.js
@@ -54,8 +54,24 @@ var Manager,
 			else {
 				settings.url += (qidx < 0 ? "?" : "&" ) + "wt=json"; 
 			}
-		}
 		},
+		exportFields: 'Ambit_InchiKey:s_uuid,doc_uuid,topcategory,endpointcategory,guidance,substanceType,name,publicname,reference,reference_owner,interpretation_result,reference_year,content,owner_name,P-CHEM.PC_GRANULOMETRY_SECTION.SIZE,CASRN.CORE,CASRN.COATING,CASRN.CONSTITUENT,CASRN.ADDITIVE,CASRN.IMPURITY,EINECS.CONSTITUENT,EINECS.ADDITIVE,EINECS.IMPURITY,ChemicalName.CORE,ChemicalName.COATING,ChemicalName.CONSTITUENT,ChemicalName.ADDITIVE,ChemicalName.IMPURITY,TradeName.CONSTITUENT,TradeName.ADDITIVE,TradeName.IMPURITY,COMPOSITION.CORE,COMPOSITION.COATING,COMPOSITION.CONSTITUENT,COMPOSITION.ADDITIVE,COMPOSITION.IMPURITY',
+		exportMaxRows: 999999 //2147483647
+		},
+		Exports = [
+	      { mime: "application/json", icon: "images/types/json64.png"},
+	      { mime: "text/csv", icon: "images/types/csv64.png"},
+	      { mime: "text/tsv", icon: "images/types/txt64.png"}
+	/*
+	      { mime: "chemical/x-cml", icon: "images/types/cml64.png"},
+	      { mime: "chemical/x-mdl-sdfile", icon: "images/types/sdf64.png"},
+	      { mime: "chemical/x-daylight-smiles", icon: "images/types/smi64.png"},
+	      { mime: "chemical/x-inchi", icon: "images/types/inchi64.png"},
+	      { mime: "text/uri-list", icon: "images/types/lnk64.png"},
+	      { mime: "application/pdf", icon: "images/types/pdf64.png"},
+	      { mime: "application/rdf+xml", icon: "images/types/rdf64.png"}
+	*/
+	    ],
 		
 	Manager = new (a$(Solr.Management, Solr.Configuring, Solr.QueryingJson, jT.Consumption, jT.RawSolrTranslation))(Settings);
 
@@ -214,17 +230,108 @@ var Manager,
 				$("footer", this).addClass("remove");
 			}			
 		});
-		
+
 		Manager.init();
 		
 		// now get the search parameters passed via URL	
 		Manager.addParameter('q', $.url().param('search') || '*:*');
 		Manager.doRequest();
 
+		// Prepare the export tab
+	    var exportEl = $("#export_tab div.data_types"),
+	        updateButton = function (e) {
+				var form = this.form,
+				b = $("button", this.form);
+
+				if (!form.export_dataset.value)
+					b.button("option", "label", "No target dataset selected...");
+				else if (!!form.export_type.value)
+					b.button("enable").button("option", "label", "Download " + $("#export_dataset :radio:checked + label").text().toLowerCase() + " as " + this.form.export_type.value.toUpperCase());
+				return b;
+	        };
+        
+		for (var i = 0, elen = Exports.length; i < elen; ++i) {
+			var el = jT.getFillTemplate("#export-type", Exports[i]);
+
+			exportEl.append(el);
+			$("a", el[0]).on("click", function (e) {
+				var me = $(this);
+				if (!me.hasClass("selected")) {
+					var form = me.closest("form")[0],
+					cont = me.closest("div.data_types"),
+					mime = me.data("mime");
+
+					form.export_type.value = mime = mime.substr(mime.indexOf("/") + 1);
+					updateButton.call(form.export_type, e);
+
+					$("div", cont[0]).removeClass("selected");
+					cont.addClass("selected");
+					me.closest(".jtox-fadable").addClass("selected");
+				}
+				return false;
+			});
+		}
+    
+		$("#export_dataset").buttonset();
+		$("#export_dataset input").on("change", updateButton);
+		$("#export_tab button").button({ disabled: true });
+		$("#export_tab form").on("submit", function (e) {
+			var form = this,
+				mime = form.export_type.value,
+				params = ['rows=' + Settings.exportMaxRows, 'fl=' + encodeURIComponent(Settings.exportFields)];
+
+			if (mime == "tsv")
+				params.push("wt=csv", "csv.separator=%09");
+			else
+				params.push('wt=' + mime);
+	      
+			if (form.export_dataset.value == "filtered") {
+				var values = Manager.parameterStore.fq;
+
+				for (var i = 0, vl = values.length; i < vl; i++) {
+					if (!values[i].value.match(/collapse/))
+					params.push('fq=' + encodeURIComponent(values[i].value));
+				}
+
+				form.q.value = Manager.parameterStore.q.value;
+			}else { // i.e. selected
+		        var fqset = [];
+		        Basket.enumerateItems(function (d) { fqset.push(d.s_uuid); });
+		        form.q.value = 's_uuid:(' + fqset.join(" ") + ')';
+			}
+
+			form.action = Manager.solrUrl + "select?" + params.join('&');
+	      
+			e.preventDefault();
+			form.submit();
+			return false;
+		});
+
+		$("#result-tabs").tabs( { 
+			activate: function (e, ui) {
+				if (ui.newPanel[0].id == 'export_tab') {
+					$("div", ui.newPanel[0]).removeClass("selected");
+					$("button", ui.newPanel[0]).button("disable").button("option", "label", "No output format selected...");
+
+					var hasFilter = Manager.parameterStore.fq.length > 1 || Manager.parameterStore.q.value != '*:*';
+
+					$("#selected_data")[0].disabled = Basket.length < 1;
+					$("#selected_data")[0].checked = Basket.length > 0 && !hasFilter;
+					$("#filtered_data")[0].disabled = !hasFilter;
+					$("#filtered_data")[0].checked = hasFilter;
+
+					$("#export_dataset").buttonset("refresh");
+				}
+			}
+		});
+		
+		
+
 		// Set some general search machanisms
 		$(document).on('click', "a.freetext_selector", function (e) {
   		Manager.addParameter('q', Solr.escapeValue(this.innerText));
   		Manager.doRequest();
 		});
+
 	});
 })(Solr, asSys, jQuery, jToxKit);
