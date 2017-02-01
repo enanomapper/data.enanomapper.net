eb6568504d765253a306f0db29f810dedff46446
tsmanolov <ts.manolov@gmail.com>
2017-02-01 11:42:20 +0200

    export ux changes

--- js/nr.js
+++ js/nr.js
@@ -1,7 +1,6 @@
 var	Settings = {
-      solrUrl: 'https://sandbox.ideaconsult.net/solr/enanondm_shard1_replica1/',
       ambitURL: 'https://apps.ideaconsult.net/nanoreg1/',
-		  solrUrl: 'https://sandbox.ideaconsult.net/solr/nanoreg1test_shard1_replica1/',
+      solrUrl: 'https://sandbox.ideaconsult.net/solr/nanoreg1ndm_shard1_replica1/',
 
     	listingFields: [ 
         "name:name_hs", 
