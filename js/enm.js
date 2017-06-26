a5e55661abf31981fc45006ef74375e174650fcb
tsmanolov <ts.manolov@gmail.com>
2017-06-26 14:51:54 +0300

    child filter

--- js/enm.js
+++ js/enm.js
@@ -42,6 +42,7 @@ var	Settings = {
             return { 'topic': topic.toLowerCase(), 'content' : pattern.replace(re, min + "&nbsp;&hellip;&nbsp;" + max) };
       		}
       },
+      studyInnerFields: "topcategory_s, endpointcategory_s, guidance_s",
       facets: [ 
     		{ id: 'owner_name', field: "owner_name_s", title: "Data sources", color: "green", facet: { mincount: 450, domain: { blockChildren: "type_s:params" } } }, 
   			{ id: 'substanceType', field: "substanceType_s", title: "Nanomaterial type", facet: { mincount: 2, limit: -1, domain: { blockChildren: "type_s:params" } } },
@@ -53,12 +54,12 @@ var	Settings = {
     		{ id: 'protocol', field: "guidance_s", title: "Protocols", color: "blue", facet: { mincount: 2, domain: { blockChildren: "type_s:params" } } },
       	],
       exportType: [
-        { type: "Material, composition and study", fields: "*", formats: "json,csv,tsv,xslx,rdf,json-ld,isa-json"},
-        { type: "Material identifiers", fields: "substance_uuid:s_uuid_hs,name:name_hs,publicname:publicname_hs,supplier:owner_name_hs,substanceType:substanceType_hs", formats: "json,csv,tsv"},
-        { type: "Material composition", fields: "substance_uuid:s_uuid_hs,[childFilter=type_s:composition limit=100] ", formats: "json"},
-        { type: "Study results", fields: "substance_uuid:s_uuid_hs,[child parentFilter=type_s:substance childFilter=type_s:study ]", formats: "json"},
-        { type: "Protocol parameters", fields: "substance_uuid:s_uuid_hs,[child parentFilter=type_s:substance childFilter=type_s:params ]", formats: "json"},
-        { type: "Study factors", fields: "substance_uuid:s_uuid_hs,[child parentFilter=type_s:substance childFilter=type_s:conditions]", formats: "json"}
+       { type: "Material, composition and study", fields: "*", childFilter:"", defaultChildFilter:"", formats: "json,csv,tsv,xslx,rdf,json-ld,isa-json"},
+        { type: "Material identifiers", fields: "substance_uuid:s_uuid_hs,name:name_hs,publicname:publicname_hs,supplier:owner_name_hs,substanceType:substanceType_hs", childFilter:"", defaultChildFilter:"", formats: "json,csv,tsv"},
+        { type: "Material composition", fields: "substance_uuid:s_uuid_hs", childFilter:"[childFilter={{childFilter}} limit=100]", defaultChildFilter:"type_s:composition", formats: "json"},
+        { type: "Study results", fields: "substance_uuid:s_uuid_hs", childFilter:"[child parentFilter=type_s:substance childFilter={{childFilter}}]", defaultChildFilter:"type_s:study", formats: "json"},
+        { type: "Protocol parameters", fields: "substance_uuid:s_uuid_hs", childFilter:"[child parentFilter=type_s:substance childFilter={{childFilter}} ]", defaultChildFilter:"type_s:params", formats: "json"},
+        { type: "Study factors", fields: "substance_uuid:s_uuid_hs", childFilter:"[child parentFilter=type_s:substance childFilter={{childFilter}}]", defaultChildFilter:"type_s:conditions", formats: "json"}
       ],
   		exportFormats: [
         { mime: "application/json", name:"json", icon: "images/types/json64.png", server: 'solrUrl'},
