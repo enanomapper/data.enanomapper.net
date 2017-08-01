38b0667396be4a3d84289fa5e63ee4e151551a45
theJonan <ivan@jonan.info>
2017-08-01 23:33:28 +0300

    - Big exporting changes.

--- js/enm.js
+++ js/enm.js
@@ -1,6 +1,6 @@
 var	Settings = {
-   		solrUrl: 'https://solr.ideaconsult.net/solr/nanoreg_shard1_replica1/',
-      ambitURL: 'https://apps.ideaconsult.net/nanoreg1/',
+   		solrUrl: 'https://sandbox.ideaconsult.net/solr/nanoreg1test_shard1_replica1/',
+      ambitUrl: 'https://apps.ideaconsult.net/nanoreg1/',
       multipleSelection: true,
       keepAllFacets: true,
       aggregateFacets: false,
@@ -14,7 +14,7 @@ var	Settings = {
         "content:content_hss",
         "SUMMARY.*"
       ],
-      dbs: {
+      tagDbs: {
         "NNRG": {
           "server": "https://apps.ideaconsult.net/nanoreg1/",
           "icon": "http://www.nanoreg.eu/images/NANoREG_Logo_short.jpg"
@@ -53,7 +53,12 @@ var	Settings = {
             return { 'topic': topic.toLowerCase(), 'content' : pattern.replace(re, min + "&nbsp;&hellip;&nbsp;" + max) };
       		}
       },
-      studyInnerFields: "topcategory_s, endpointcategory_s, guidance_s",
+      pivot: [ 
+			  { id: "topcategory", field: "topcategory_s", disabled: true, facet: { domain: { blockChildren: "type_s:(params conditions)" } } },
+			  { id: "endpointcategory", field: "endpointcategory_s", color: "blue" },
+			  { id: "effectendpoint", field: "effectendpoint_s", color: "green", ranging: true }, 
+			  { id: "unit", field: "unit_s", disabled: true, ranging: true }
+  	  ],
       facets: [ 
     		{ id: 'owner_name', field: "owner_name_s", title: "Data sources", color: "green", facet: { mincount: 450, domain: { blockChildren: "type_s:params" } } }, 
   			{ id: 'substanceType', field: "substanceType_s", title: "Nanomaterial type", facet: { mincount: 2, limit: -1, domain: { blockChildren: "type_s:params" } } },
@@ -64,22 +69,48 @@ var	Settings = {
     		{ id: 'reference', field: "reference_s", title: "References", facet: { mincount: 2, domain: { blockChildren: "type_s:params" } } }, 
     		{ id: 'protocol', field: "guidance_s", title: "Protocols", color: "blue", facet: { mincount: 2, domain: { blockChildren: "type_s:params" } } },
       	],
-      exportType: [
-       { type: "Material, composition and study", fields: "*", childFilter:"", defaultChildFilter:"", formats: "json,csv,tsv,xslx,rdf,json-ld,isa-json"},
-        { type: "Material identifiers", fields: "substance_uuid:s_uuid_hs,name:name_hs,publicname:publicname_hs,supplier:owner_name_hs,substanceType:substanceType_hs", childFilter:"", defaultChildFilter:"", formats: "json,csv,tsv"},
-        { type: "Material composition", fields: "substance_uuid:s_uuid_hs", childFilter:"[childFilter={{childFilter}} limit=100]", defaultChildFilter:"type_s:composition", formats: "json"},
-        { type: "Study results", fields: "substance_uuid:s_uuid_hs", childFilter:"[child parentFilter=type_s:substance childFilter={{childFilter}}]", defaultChildFilter:"type_s:study", formats: "json"},
-        { type: "Protocol parameters", fields: "substance_uuid:s_uuid_hs", childFilter:"[child parentFilter=type_s:substance childFilter={{childFilter}} ]", defaultChildFilter:"type_s:params", formats: "json"},
-        { type: "Study factors", fields: "substance_uuid:s_uuid_hs", childFilter:"[child parentFilter=type_s:substance childFilter={{childFilter}}]", defaultChildFilter:"type_s:conditions", formats: "json"}
+      studyInnerFields: "topcategory_s,endpointcategory_s,guidance_s",
+      exportTypes: [
+        { 
+          name: "Material, composition and study",
+          fields: "substance_uuid:s_uuid_hs,name:name_hs,publicname:publicname_hs,supplier:owner_name_hs,substanceType:substanceType_hs,[child parentFilter=type_s:substance childFilter=\"{{filter}} OR type_s:composition\" limit=10000]",
+          defaultFilter:"type_s:study",
+          formats: "json,csv,tsv,xslx,rdf,json-ld,isa-json",
+        }, { 
+          name: "Material identifiers",
+          fields: "substance_uuid:s_uuid_hs,name:name_hs,publicname:publicname_hs,supplier:owner_name_hs,substanceType:substanceType_hs",
+          formats: "json,csv,tsv"
+        }, { 
+          name: "Material composition",
+          fields: "substance_uuid:s_uuid_hs,[child parentFilter=type_s:substance childFilter=type_s:composition limit=1000]",
+          formats: "json,csv,tsv"
+        }, { 
+          name: "Study results",
+          fields: "substance_uuid:s_uuid_hs,[child parentFilter=type_s:study limit=10000]",
+          extraParams: [ "group.field=s_uuid_s" ],
+          formats: "json,csv,tsv",
+          nestLevel: "study"
+        }, { 
+          name: "Protocol parameters",
+          fields: "substance_uuid:s_uuid_hs,[child parentFilter=type_s:study childFilter=type_s:params]",
+          extraParams: [ "group.field=s_uuid_s" ],
+          formats: "json,csv,tsv",
+          nestLevel: "study"
+        }, { 
+          name: "Study factors",
+          fields: "substance_uuid:s_uuid_hs,[child parentFilter=type_s:study childFilter=type_s:conditions]",
+          extraParams: [ "group.field=s_uuid_s" ],
+          formats: "json,csv,tsv",
+          nestLevel: "study"
+        }
       ],
   		exportFormats: [
         { mime: "application/json", name:"json", icon: "images/types/json64.png", server: 'solrUrl'},
         { mime: "text/csv", name:"csv", icon: "images/types/csv64.png", server: 'solrUrl'},
         { mime: "text/tsv", name:"tsv", icon: "images/types/txt64.png", server: 'solrUrl'},
-        { mime: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", name:"xslx", icon: "images/types/xlsx.png", server: 'ambitURL'},
-        { mime: "application/rdf+xml", name:"rdf", icon: "images/types/rdf64.png", server: 'ambitURL'},
-        { mime: "application/ld+json", name:"json-ld", icon: "images/types/json-ld.png", server: 'ambitURL'},
-        { mime: "application/isa+json", name:"isa-json", icon: "images/types/isa.png", server: 'ambitURL'}
- 
+        { mime: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", name:"xslx", icon: "images/types/xlsx.png", server: 'ambitUrl'},
+        { mime: "application/rdf+xml", name:"rdf", icon: "images/types/rdf64.png", server: 'ambitUrl'},
+        { mime: "application/ld+json", name:"json-ld", icon: "images/types/json-ld.png", server: 'ambitUrl'},
+        { mime: "application/isa+json", name:"isa-json", icon: "images/types/isa.png", server: 'ambitUrl'}
       ]
 		};
--- js/nr.js
+++ js/nr.js
@@ -1,6 +1,5 @@
 var	Settings = {
-      ambitURL: 'https://apps.ideaconsult.net/nanoreg1/',
-
+      ambitUrl: 'https://apps.ideaconsult.net/nanoreg1/',
       solrUrl: 'https://solr.ideaconsult.net/solr/nanoreg_shard1_replica1/',
 
     	listingFields: [ 
@@ -42,6 +41,12 @@ var	Settings = {
             return { 'topic': topic.toLowerCase(), 'content' : pattern.replace(re, min + "&nbsp;&hellip;&nbsp;" + max) };
       		}
       },
+      pivot: [ 
+			  { id: "topcategory", field: "topcategory_s", disabled: true, facet: { domain: { blockChildren: "type_s:params" } } },
+			  { id: "endpointcategory", field: "endpointcategory_s", color: "blue" },
+			  { id: "effectendpoint", field: "effectendpoint_s", color: "green", ranging: true }, 
+			  { id: "unit", field: "unit_s", disabled: true, ranging: true }
+  	  ],
       facets: [ 
     		{ id: 'owner_name', field: "reference_owner_s", title: "Data sources", color: "green", facet: { mincount: 1 , domain: { blockChildren: "type_s:params" } } }, 
   			{ id: 'substanceType', field: "substanceType_s", title: "Nanomaterial type", facet: { mincount: 1, domain: { blockChildren: "type_s:params" } } },
@@ -57,22 +62,23 @@ var	Settings = {
   			{ id: 'protocol', field: "guidance_s", title: "Protocols", color: "blue", facet: { mincount: 1 , domain: { blockChildren: "type_s:params" }} },
 				{ id: 'method', field: "E.method_s", title: "Method", color: "green", facet: { mincount: 1 , domain: { blockChildren: "type_s:params" }} } 
     	],
-      exportType: [
-        { type: "Material, composition and study", fields: "*", formats: "json,csv,tsv,xslx,rdf,json-ld,isa-json"},
-        { type: "Material identifiers", fields: "substance_uuid:s_uuid_hs,name:name_hs,publicname:publicname_hs,supplier:owner_name_hs,substanceType:substanceType_hs", formats: "json,csv,tsv"},
-        { type: "Material composition", fields: "substance_uuid:s_uuid_hs,[childFilter=type_s:composition limit=100] ", formats: "json"},
-        { type: "Study results", fields: "substance_uuid:s_uuid_hs,[child parentFilter=type_s:substance childFilter=type_s:study ]", formats: "json"},
-        { type: "Protocol parameters", fields: "substance_uuid:s_uuid_hs,[child parentFilter=type_s:substance childFilter=type_s:params ]", formats: "json"},
-        { type: "Study factors", fields: "substance_uuid:s_uuid_hs,[child parentFilter=type_s:substance childFilter=type_s:conditions]", formats: "json"}
+      studyInnerFields: "topcategory_s, endpointcategory_s, guidance_s",
+      exportTypes: [
+        { name: "Material, composition and study", fields: "*", formats: "json,csv,tsv,xslx,rdf,json-ld,isa-json"},
+        { name: "Material identifiers", fields: "substance_uuid:s_uuid_hs,name:name_hs,publicname:publicname_hs,supplier:owner_name_hs,substanceType:substanceType_hs", formats: "json,csv,tsv"},
+        { name: "Material composition", fields: "substance_uuid:s_uuid_hs,[childFilter=type_s:composition limit=100] ", formats: "json"},
+        { name: "Study results", fields: "substance_uuid:s_uuid_hs,[child parentFilter=type_s:substance childFilter=type_s:study ]", formats: "json"},
+        { name: "Protocol parameters", fields: "substance_uuid:s_uuid_hs,[child parentFilter=type_s:substance childFilter=type_s:params ]", formats: "json"},
+        { name: "Study factors", fields: "substance_uuid:s_uuid_hs,[child parentFilter=type_s:substance childFilter=type_s:conditions]", formats: "json"}
       ],
   		exportFormats: [
         { mime: "application/json", name:"json", icon: "images/types/json64.png", server: 'solrUrl'},
         { mime: "text/csv", name:"csv", icon: "images/types/csv64.png", server: 'solrUrl'},
         { mime: "text/tsv", name:"tsv", icon: "images/types/txt64.png", server: 'solrUrl'},
-        { mime: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", name:"xslx", icon: "images/types/xlsx.png", server: 'ambitURL'},
-        { mime: "application/rdf+xml", name:"rdf", icon: "images/types/rdf64.png", server: 'ambitURL'},
-        { mime: "application/ld+json", name:"json-ld", icon: "images/types/json-ld.png", server: 'ambitURL'},
-        { mime: "application/isa+json", name:"isa-json", icon: "images/types/isa.png", server: 'ambitURL'}
+        { mime: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", name:"xslx", icon: "images/types/xlsx.png", server: 'ambitUrl'},
+        { mime: "application/rdf+xml", name:"rdf", icon: "images/types/rdf64.png", server: 'ambitUrl'},
+        { mime: "application/ld+json", name:"json-ld", icon: "images/types/json-ld.png", server: 'ambitUrl'},
+        { mime: "application/isa+json", name:"isa-json", icon: "images/types/isa.png", server: 'ambitUrl'}
  
       ]
 		};
